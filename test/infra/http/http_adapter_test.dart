b89ea9ffb92e957b1fbdd3fb7e07e1277859a350
Gabriel Kohler <85637999+gabriel-kohler@users.noreply.github.com>
2021-09-23 19:36:45 -0300

    test(infra): ensure HttpAdapter calls post with correct headers

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,4 +1,5 @@
 import 'package:faker/faker.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
@@ -10,8 +11,17 @@ class HttpAdapter {
 
   HttpAdapter(this.client);
 
-  Future<void> request(String url) async {
-    await client.post(Uri.parse(url));
+  Future<void> request({
+    @required String url,
+    @required String method,
+    Map body,
+  }) async {
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json',
+    };
+
+    await client.post(Uri.parse(url), headers: headers);
   }
 }
 
@@ -22,9 +32,15 @@ void main() {
       final sut = HttpAdapter(client);
       final url = faker.internet.httpUrl();
 
-      sut.request(url);
+      await sut.request(url: url, method: 'post');
 
-      verify(client.post(Uri.parse(url)));
+      verify(client.post(
+        Uri.parse(url),
+        headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json',
+        },
+      ));
     });
   });
 }
