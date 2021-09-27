69b4f9334d2f5f0baafe432c017220920c7b481a
Gabriel Kohler <85637999+gabriel-kohler@users.noreply.github.com>
2021-09-27 10:07:09 -0300

    refactor(infra): move generic code to a private method

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart';
 
 import '/data/http/http.dart';
 
-class HttpAdapter implements HttpClient{
+class HttpAdapter implements HttpClient {
   final Client client;
 
   HttpAdapter(this.client);
@@ -21,14 +21,27 @@ class HttpAdapter implements HttpClient{
       'accept': 'application/json',
     };
 
-    final jsonBody = body != null ? jsonEncode(body) : null;
+    final jsonBody = _jsonBody(body);
 
     final response = await client.post(Uri.parse(url), headers: headers, body: jsonBody);
 
+    return _handleResponse(response);
+  }
+
+  String _jsonBody(Map body) {
+    if (body != null) {
+      return jsonEncode(body);
+    } else {
+      return null;
+    }
+  }
+
+  Map _handleResponse(Response response) {
     if (response.statusCode == 200) {
       return response.body.isNotEmpty ? jsonDecode(response.body) : null;
     } else {
       return null;
     }
   }
+  
 }
