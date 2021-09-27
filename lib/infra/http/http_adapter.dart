d78d66ad635a462e995b3aa0aefd3a85cb98b14e
Gabriel Kohler <85637999+gabriel-kohler@users.noreply.github.com>
2021-09-27 16:22:53 -0300

    test(infra): ensure HttpAdapter throws NotFoundError if post returns 404

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -47,8 +47,10 @@ class HttpAdapter implements HttpClient {
       throw HttpError.serverError;
     } else if (response.statusCode == 401) {
       throw HttpError.unauthorized;
-    } else {
+    } else if (response.statusCode == 403) {
       throw HttpError.forbidden;
+    } else {
+      throw HttpError.notFound;
     }
   }
   
