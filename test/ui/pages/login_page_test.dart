9661f0d775d1b8e3d2891e810d5a0d539ed828b7
Gabriel Kohler <85637999+gabriel-kohler@users.noreply.github.com>
2021-10-01 14:33:05 -0300

    test(ui): move page initialization to a helper method

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -1,33 +1,40 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:practice/ui/pages/pages.dart';
+import 'package:practice/ui/pages/login_page.dart';
 
 void main() {
-  testWidgets('Should load with correct initial state',
-      (WidgetTester tester) async {
+  Future<void> loadPage(WidgetTester tester) async {
     final loginPage = MaterialApp(home: LoginPage());
     await tester.pumpWidget(loginPage);
+  }
 
-    final emailTextChildren = find.descendant(of: find.bySemanticsLabel('Email'), matching: find.byType(Text));
+  testWidgets('Should load with correct initial state',
+      (WidgetTester tester) async {
 
-    expect(
-      emailTextChildren,
-      findsOneWidget,
-      reason:'when a TextFormField has only one text child, means it has no errors, since one of the childs is always the label text',
+    await loadPage(tester);
+
+    //emailTextChildren = captura os filhos do tipo text do componente que contem uma String com valor "Email"
+    //por padrão um textformfield sempre vai ter um filho do tipo text, que é o labelText, se ele tiver mais um é porque
+    //o segundo filho do tipo text corresponde ao errorText, ou seja, ele tem um erro
+    // seguindo a lógica, se o textformfield tem mais de um filho do tipo text, é porque ele tem um erro
+    final emailTextChildren = find.descendant(
+      of: find.bySemanticsLabel('Email'),
+      matching: find.byType(Text),
     );
 
-    final passwordTextChildren = find.descendant(of: find.bySemanticsLabel('Senha'), matching: find.byType(Text));
+    expect(emailTextChildren, findsOneWidget);
 
-    expect(
-      passwordTextChildren,
-      findsOneWidget,
-      reason: 'when a TextFormField has only one text child, means it has no errors, since one of the childs is always the label text',
+    final passwordTextChildren = find.descendant(
+      of: find.bySemanticsLabel('Senha'),
+      matching: find.byType(Text),
     );
 
-    final loginButton = tester.widget<ElevatedButton>(find.byType(ElevatedButton));
-    
-    expect(loginButton.onPressed, null);
+    expect(passwordTextChildren, findsOneWidget);
+
+    final elevatedButtonLogin = tester.widget<ElevatedButton>(
+      find.byType(ElevatedButton),
+    );
+
+    expect(elevatedButtonLogin.onPressed, null);
   });
 }
