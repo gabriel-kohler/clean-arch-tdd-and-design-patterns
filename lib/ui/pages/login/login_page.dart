4e5a0035e688a75ef1b7100956ce45a316f39c22
Gabriel Kohler <85637999+gabriel-kohler@users.noreply.github.com>
2021-10-11 10:03:54 -0300

    refactor(ui): move SpinnerDialog to its own file

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -25,31 +25,9 @@ class _LoginPageState extends State<LoginPage> {
         widget.loginPresenter.isLoadingStream.listen(
           (isLoading) {
             if (isLoading) {
-              showDialog(
-                context: context,
-                barrierDismissible: false,
-                builder: (BuildContext context) {
-                  return SimpleDialog(
-                    children: <Widget>[
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          CircularProgressIndicator(),
-                          SizedBox(height: 10),
-                          Text(
-                            'Aguarde...',
-                            textAlign: TextAlign.center,
-                          ),
-                        ],
-                      ),
-                    ],
-                  );
-                },
-              );
+              showLoading(context);
             } else {
-              if (Navigator.canPop(context)) {
-                Navigator.of(context).pop();
-              }
+              hideLoading(context);
             }
           },
         );
