0d34d4f755a40d2ffecc7aa2ed87e395e0a593f1
Gabriel Kohler <85637999+gabriel-kohler@users.noreply.github.com>
2021-10-11 09:51:05 -0300

    refactor(ui): add type to streams

--- lib/ui/pages/login/login_presenter.dart
+++ lib/ui/pages/login/login_presenter.dart
@@ -2,9 +2,9 @@ abstract class LoginPresenter {
 
   Stream<String> get emailErrorStream;
   Stream<String> get passsowrdErrorStream;
+  Stream<String> get mainErrorStream;
   Stream<bool> get isFormValidStream;
   Stream<bool> get isLoadingStream;
-  Stream<String> get mainErrorStream;
 
   void validateEmail(String email);
   void validatePassword(String password);
