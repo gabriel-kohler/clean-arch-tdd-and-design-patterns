61453c3695ed18eaf389465b91267b004fc1de80
Gabriel Kohler <85637999+gabriel-kohler@users.noreply.github.com>
2021-10-14 21:24:37 -0300

    test(presentation): ensure EmailErrorStream will not emit duplicated event

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -8,12 +8,13 @@ import 'package:practice/presentation/presenters/presenters.dart';
 class ValidationSpy extends Mock implements Validation {}
 
 void main() {
-
   ValidationSpy validation;
   StreamLoginPresenter sut;
   String email;
 
-  PostExpectation mockValidationCall(String field) => when(validation.validate(field: field == null ? anyNamed('field') : field, value: anyNamed('value')));
+  PostExpectation mockValidationCall(String field) => when(validation.validate(
+      field: field == null ? anyNamed('field') : field,
+      value: anyNamed('value')));
 
   void mockValidation({String field, String value}) {
     mockValidationCall(field).thenReturn(value);
@@ -27,21 +28,23 @@ void main() {
   });
 
   test('Should call validation with correct validation', () {
-
     sut.validateEmail(email);
 
     verify(validation.validate(field: 'email', value: email)).called(1);
   });
 
   test('Should emit email error if validation fails', () {
-   
     mockValidation(value: 'error');
+
+    sut.emailErrorStream.listen(
+      expectAsync1((error) {
+        expect(error, 'error');
+      }),
+    );
 
     expectLater(sut.emailErrorStream, emits('error'));
 
     sut.validateEmail(email);
+
   });
-
-   
-
 }
