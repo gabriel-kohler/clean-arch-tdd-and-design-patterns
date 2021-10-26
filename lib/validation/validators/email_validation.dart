8367094755a03917398bfc2308a58033661026df
Gabriel Kohler <85637999+gabriel-kohler@users.noreply.github.com>
2021-10-26 19:58:17 -0300

    feat(validation): move EmailValidation to its own file

---
+++ lib/validation/validators/email_validation.dart
@@ -0,0 +1,17 @@
+
+import '/validation/dependencies/dependencies.dart';
+
+class EmailValidation implements FieldValidation {
+  final String field;
+
+  EmailValidation(this.field);
+
+  @override
+  String validate(String value) {
+    final regex = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+
+    final isValid = value?.isNotEmpty != true || regex.hasMatch(value);
+
+    return isValid ? null : 'Campo inválido';
+  }
+}
--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -1,20 +1,5 @@
-import 'package:practice/validation/dependencies/dependencies.dart';
+import 'package:practice/validation/validators/validators.dart';
 import 'package:test/test.dart';
-
-class EmailValidation implements FieldValidation {
-  final String field;
-
-  EmailValidation(this.field);
-
-  @override
-  String validate(String value) {
-    final regex = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
-
-    final isValid = value?.isNotEmpty != true || regex.hasMatch(value);
-
-    return isValid ? null : 'Campo inválido';
-  }
-}
 
 void main() {
 
