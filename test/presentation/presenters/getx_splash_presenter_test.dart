f25de8d4e38d7a45e4b4c75cd58fe425c78cda5d
Gabriel Kohler <85637999+gabriel-kohler@users.noreply.github.com>
2021-12-13 19:52:00 -0300

    test(presentation): ensure GetxSplashPresenter navigate to home page if have token on cache

--- test/presentation/presenters/getx_splash_presenter_test.dart
+++ test/presentation/presenters/getx_splash_presenter_test.dart
@@ -1,4 +1,8 @@
+import 'package:get/get.dart';
 import 'package:mockito/mockito.dart';
+import 'package:practice/domain/entities/entities.dart';
+import 'package:practice/ui/pages/pages.dart';
+import 'package:practice/utils/app_routes.dart';
 import 'package:test/test.dart';
 import 'package:meta/meta.dart';
 
@@ -6,13 +10,19 @@ import 'package:practice/domain/usecases/load_current_account.dart';
 
 class LoadCurrentAccountSpy extends Mock implements LoadCurrentAccount {}
 
-class GetxSplashPresenter {
+class GetxSplashPresenter extends GetxController implements SplashPresenter {
   final LoadCurrentAccount loadCurrentAccount;
 
   GetxSplashPresenter({@required this.loadCurrentAccount});
 
-  Future<void> loadAccount() async {
+  var _navigateTo = RxString(null);
+  
+  @override
+  Stream<String> get navigateToStream => _navigateTo.stream;
+
+  Future<void> checkAccount() async {
     await loadCurrentAccount.fetch();
+    _navigateTo.value = AppRoute.HomePage;
   }
 
 }
@@ -28,8 +38,23 @@ void main() {
 
   test('Should SplashPresenter calls LoadCurrentAccount with correct values', () async {
     
-    await sut.loadAccount();
+    await sut.checkAccount();
 
     verify(loadCurrentAccountSpy.fetch()).called(1);
   });
+
+  test('Should SplashPresenter navigate to home page if have token', () async {
+
+    when(loadCurrentAccountSpy.fetch()).thenAnswer((_) async => AccountEntity('any_token'));
+
+    sut.navigateToStream.listen(
+      expectAsync1((page) {
+        expect(page, AppRoute.HomePage);
+      })
+    );
+
+    await sut.checkAccount();
+
+  });
+
 }
