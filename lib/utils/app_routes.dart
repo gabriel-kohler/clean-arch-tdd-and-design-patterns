801614397d908e1421044a59b06a6cf017291cf4
Gabriel Kohler <85637999+gabriel-kohler@users.noreply.github.com>
2021-12-13 19:15:57 -0300

    test(ui): ensure SplashPage calls CheckAccount

---
+++ lib/utils/app_routes.dart
@@ -0,0 +1,5 @@
+class AppRoute {
+  static const SplashPage = '/';
+  static const LoginPage = '/login';
+  static const HomePage = '/home';
+}
--- test/ui/pages/splash_page_test.dart
+++ test/ui/pages/splash_page_test.dart
@@ -1,38 +1,88 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
+import 'package:mockito/mockito.dart';
+import 'package:practice/utils/app_routes.dart';
+
+abstract class SplashPresenter {
+
+  Stream<String> get navigateToStream;
+
+  Future<void> checkAccount();
+}
 
 class SplashPage extends StatelessWidget {
+
+  final SplashPresenter splashPresenter;
+
+  SplashPage({@required this.splashPresenter});
+
   @override
   Widget build(BuildContext context) {
+    splashPresenter.checkAccount();
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: CircularProgressIndicator(),
-        ),
+      body: Builder(
+        builder: (context) {
+          splashPresenter.navigateToStream.listen((page) {
+            if (page?.isNotEmpty == true) {
+              Get.offAllNamed(page);
+            }
+          });
+          return Container(
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
       ),
     );
   }
 }
 
-// navigationStream
-// AccountEntity loadAccount
+class SplashPresenterSpy extends Mock implements SplashPresenter {}
 
 void main() {
+
+  SplashPresenter splashPresenterSpy;
+  
+  StreamController<String> navigationToController;
+
   Future<void> loadPage(WidgetTester tester) async {
+
+    splashPresenterSpy = SplashPresenterSpy();
+
+    navigationToController = StreamController<String>();
+
+    when(splashPresenterSpy.navigateToStream).thenAnswer((_) => navigationToController.stream);
+
     final splashPage = GetMaterialApp(
-      initialRoute: '/splash',
+      initialRoute: AppRoute.SplashPage,
       getPages: [
-        GetPage(name: '/splash', page: () => SplashPage()),
+        GetPage(name: AppRoute.SplashPage, page: () => SplashPage(splashPresenter: splashPresenterSpy)),
+        GetPage(name: '/any_route', page: () => Scaffold(
+          body: Text('navigation test'),
+          ),
+        ),
       ],
     );
     await tester.pumpWidget(splashPage);
   }
 
+  tearDown(() {
+    navigationToController.close();
+  });
+
   testWidgets('Should SplashPage start with correct initial state', (WidgetTester tester) async {
     await loadPage(tester);
 
     expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+  testWidgets('Should SplashPage call CheckAccount', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    verify(splashPresenterSpy.checkAccount()).called(1);
   });
 
 }
