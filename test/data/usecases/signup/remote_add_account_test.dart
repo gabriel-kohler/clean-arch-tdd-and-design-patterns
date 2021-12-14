1ad049edd1ad55c34a1466e93d391c31aa6eba61
Gabriel Kohler <85637999+gabriel-kohler@users.noreply.github.com>
2021-12-14 15:55:16 -0300

    test(data): ensure RemoteAddAccount throw UnexpetedError if HttpClient returns 404

--- test/data/usecases/signup/remote_add_account_test.dart
+++ test/data/usecases/signup/remote_add_account_test.dart
@@ -94,4 +94,13 @@ void main() {
     expect(future, throwsA(DomainError.unexpected));
   });
 
+  test('Should throw UnexpectedError if HttpClient returns 404', () {
+
+    mockHttpError(HttpError.notFound);
+
+    final future = sut.add(params: params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
 }
