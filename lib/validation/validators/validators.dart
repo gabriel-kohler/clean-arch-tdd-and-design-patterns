a951a7169ee44970ea59811b47b1c943a0637acd
Gabriel Kohler <85637999+gabriel-kohler@users.noreply.github.com>
2021-12-16 00:43:07 -0300

    feat(validation): move production code to its own file

--- lib/validation/validators/validators.dart
+++ lib/validation/validators/validators.dart
@@ -1,3 +1,4 @@
 export './required_field_validation.dart';
 export './email_validation.dart';
 export './validation_composite.dart';
+export './compare_field_validation.dart';
--- test/validation/validators/compare_field_validation_test.dart
+++ test/validation/validators/compare_field_validation_test.dart
@@ -1,27 +1,7 @@
-import 'package:meta/meta.dart';
 import 'package:test/test.dart';
 
-import 'package:practice/validation/dependencies/dependencies.dart';
-import 'package:practice/presentation/dependencies/validation.dart';
-
-class CompareFieldValidation implements FieldValidation {
-
-  final String field;
-  final String valueToCompare;
-
-  CompareFieldValidation({@required this.field, @required this.valueToCompare});
-
-  @override
-  ValidationError validate({@required String value}) {
-    if (value == valueToCompare) {
-      return null;
-    } else {
-      return ValidationError.invalidField;
-    }
-  }
-
-}
-
+import 'package:practice/presentation/dependencies/dependencies.dart';
+import 'package:practice/validation/validators/validators.dart';
 void main() {
 
   CompareFieldValidation sut;
