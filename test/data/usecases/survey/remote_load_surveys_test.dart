859349b71b8630d7d681b1a69879d3bc16ef9d22
Gabriel Kohler <85637999+gabriel-kohler@users.noreply.github.com>
2021-12-18 19:17:15 -0300

    test(data): ensure RemoteLoadSurveys call HttpClient with correct values

---
+++ test/data/usecases/survey/remote_load_surveys_test.dart
@@ -0,0 +1,35 @@
+import 'package:faker/faker.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+import 'package:meta/meta.dart';
+
+import 'package:practice/data/http/http.dart';
+
+
+class RemoteLoadSurveys {
+
+  final HttpClient httpClient;
+  final String url;
+
+  RemoteLoadSurveys({@required this.httpClient, @required this.url});
+
+  Future<void> load() async {
+    await httpClient.request(url: url, method: 'get');
+  }
+
+}
+
+class HttpClientSpy extends Mock implements HttpClient {}
+void main() {
+
+  test('Should call HttpClient with correct values', () async {
+    final httpClient = HttpClientSpy();
+    final url = faker.internet.httpUrl();
+    final sut = RemoteLoadSurveys(httpClient: httpClient, url: url);
+
+    await sut.load();
+
+    verify(httpClient.request(url: url, method: 'get')).called(1);
+
+  });
+}
