84400b0ee27d34724f09e16e55a6ecff18cb6d14
Gabriel Kohler <85637999+gabriel-kohler@users.noreply.github.com>
2021-12-19 18:00:39 -0300

    test(presentation): ensure GetxSurveysPresenter show and hide loading

--- lib/presentation/presenters/getx_surveys_presenter.dart
+++ lib/presentation/presenters/getx_surveys_presenter.dart
@@ -7,9 +7,15 @@ class GetxSurveysPresenter extends GetxController {
 
   final LoadSurveys loadSurveys;
 
+  var isLoading = false.obs;
+
+  Stream<bool> get isLoadingStream => isLoading.stream;
+
   GetxSurveysPresenter({@required this.loadSurveys});
 
   Future<void> loadData() async {
+    isLoading.value = true;
     await loadSurveys.load();
+    isLoading.value = false;
   }
 }
--- test/presentation/presenters/getx_survey_presenter_test.dart
+++ test/presentation/presenters/getx_survey_presenter_test.dart
@@ -6,14 +6,27 @@ import 'package:practice/presentation/presenters/presenters.dart';
 
 class LoadSurveysSpy extends Mock implements LoadSurveys {}
 void main() {
+  LoadSurveys loadSurveysSpy;
+  GetxSurveysPresenter sut;
+
+  setUp(() {
+    loadSurveysSpy = LoadSurveysSpy();
+    sut = GetxSurveysPresenter(loadSurveys: loadSurveysSpy);
+  });
 
   test('Should call LoadSurveys with correct values', () async {
-    final loadSurveysSpy = LoadSurveysSpy();
-    final sut = GetxSurveysPresenter(loadSurveys: loadSurveysSpy);
 
     await sut.loadData();
 
     verify(loadSurveysSpy.load()).called(1);
+
+  });
+
+  test('Should present loading before call LoadSurveys and hide loading after LoadSurveys', () async {
+
+    expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+
+    await sut.loadData();
 
   });
 }
