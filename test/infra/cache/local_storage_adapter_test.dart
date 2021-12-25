f22eedf2da531c4356053453a2ad4800255bec58
Gabriel Kohler <85637999+gabriel-kohler@users.noreply.github.com>
2021-12-25 09:33:58 -0300

    feat(infra): move production code to its own file

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -1,29 +1,10 @@
 import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-import 'package:meta/meta.dart';
 
 import 'package:localstorage/localstorage.dart';
 
-class LocalStorageAdapter {
-
-  final LocalStorage localStorage;
-
-  LocalStorageAdapter({@required this.localStorage});
-
-  Future<void> save({@required String key, @required dynamic value}) async {
-    await localStorage.deleteItem(key);
-    await localStorage.setItem(key, value);
-  }
-
-  Future<void> delete({@required String key}) async {
-    await localStorage.deleteItem(key);
-  }
-
-  Future<dynamic> fetch({@required String key}) async {
-    return await localStorage.getItem(key);
-  }
-}
+import 'package:practice/infra/cache/cache.dart';
 
 class LocalStorageSpy extends Mock implements LocalStorage {}
 
