0414e539120dd3e28ac9f229f37e26f68a2a817d
Gabriel Kohler <85637999+gabriel-kohler@users.noreply.github.com>
2021-12-28 05:02:22 -0300

    test: ensure SurveysPage not change page on null, empty route

--- test/ui/pages/surveys_page_test.dart
+++ test/ui/pages/surveys_page_test.dart
@@ -133,4 +133,18 @@ void main() {
     expect(find.text('navigation test'), findsOneWidget);
   });
 
+  testWidgets('Should not change page', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    navigateToController.add('');
+    await tester.pump();
+    expect(Get.currentRoute, AppRoute.SurveysPage);
+
+    navigateToController.add(null);
+    await tester.pump();
+    expect(Get.currentRoute, AppRoute.SurveysPage);
+  });
+
+
+
 }
