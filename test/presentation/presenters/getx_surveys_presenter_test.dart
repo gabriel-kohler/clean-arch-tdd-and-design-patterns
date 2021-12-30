d61d5baf7c8fc011fbc59e6846c53a4496d25759
Gabriel Kohler <85637999+gabriel-kohler@users.noreply.github.com>
2021-12-30 01:45:08 -0300

    fix: allows navigation stream to send the same event twice

--- test/presentation/presenters/getx_surveys_presenter_test.dart
+++ test/presentation/presenters/getx_surveys_presenter_test.dart
@@ -92,12 +92,18 @@ void main() {
 
   test('Should go to SurveyResultPage on link click', () async {
     
+    expectLater(sut.navigateToStream, emitsInOrder([
+      '/survey_result/1',
+      '/survey_result/1',
+    ]));
+
     sut.navigateToStream.listen(
       expectAsync1((page) {
         expect(page, '/survey_result/1');
       }),
     );
 
+    sut.goToSurveyResult('1');
     sut.goToSurveyResult('1');
 
   });
